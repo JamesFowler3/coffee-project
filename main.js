f59f0e1f4ae3411596320f73effd9fff7034bcb7
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-07 14:36:09 -0600

    finished

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
+    html += '<h1>' + coffee.name + '</h1>';
+    html += '<p>' + coffee.roast + '</p>';
     html += '</tr>';
 
     return html;
@@ -18,18 +17,59 @@ function renderCoffees(coffees) {
     return html;
 }
 
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
+// add event selector to drop down so I can get the value and console.log it
+
+function updateCoffees(roast) {
+    event.preventDefault(); // don't submit the form, we just want to update the data
+
+    var input = document.getElementById("search");
+    var lowercaseInput = input.value.toLowerCase();
     var filteredCoffees = [];
+
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === roast) {
             filteredCoffees.push(coffee);
         }
     });
+    // put the coffees on the page
     tbody.innerHTML = renderCoffees(filteredCoffees);
-}
 
+
+}
+    function searchCoffees () {
+       event.preventDefault();
+        var input = document.getElementById("search");
+        var lowercaseInput = input.value.toLowerCase();
+        console.log("hello");
+        console.log(lowercaseInput);
+
+        var filteredCoffees = []
+
+        coffees.forEach(function (coffee) {
+            var lowercaseCoffees = coffee.name.toLowerCase();
+            // console.log(coffee.name);
+            // console.log(lowercaseCoffees.indexOf(lowercaseInput));
+            if (lowercaseCoffees.indexOf(lowercaseInput) !== -1 ) {
+                filteredCoffees.push(coffee);
+
+            }
+        });
+        console.log(filteredCoffees);
+        tbody.innerHTML = renderCoffees(filteredCoffees);
+    }
+
+// we need to get the user input
+// we need to determine if the user input is in our coffees
+// we can do this using .indexOf and forEach
+// so, forEach (coffee).indexOf(coffee.name);
+
+// indexOf: target.indexOf("Li") !== -1 => 0
+
+var roastSelection = document.querySelector('#roast-selection');
+roastSelection.addEventListener("change", function (e) {
+        var roast = e.target.value;
+        updateCoffees(roast);
+});
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -48,10 +88,15 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+// var lowercaseInput = input.toLowerCase();
+
+
+
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+
+submitButton.addEventListener('click', searchCoffees);
